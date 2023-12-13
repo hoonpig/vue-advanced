691954d2bc0156cef4503e8f12b03826e7ed635e
hoonpig <hoonpig@nate.com>
2023-12-13 22:18:32 +0900

    Vue study

--- vue-news/src/router/index.js
+++ vue-news/src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-
+import NewsView from '../views/NewsView.vue'
+import JobsView from '../views/JobsView.vue'
+import AskView from '../views/AskView.vue'
 Vue.use(VueRouter);
 
-const router = new VueRouter({
-    routes :[{
-        path:''
-        , component : ''
-    }
-        
+export const router = new VueRouter({
+    routes :[
+        {   
+            // path : url주소
+            path:'/news'
+            // component : url주소로 갔을때 표시될 컴포넌트
+            , component : NewsView
+        },
+        {
+            path:'/ask'
+            , component : JobsView
+        }   ,
+        {
+            path:'/jobs'
+            , component : AskView
+        }      
     ]
 })
-
-export default;
--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <div>jobs</div>
 </template>
 
 <script>
