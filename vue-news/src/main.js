31a0bc53d62f54fa1466a6f2aebea98b550b50a9
hoonpig <hoonpig@nate.com>
2024-01-29 22:24:36 +0900

    뷰 공부중

--- vue-news/src/main.js
+++ vue-news/src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
-import {router} from './routes/index.js'
-import App from './App.vue'
-
+import Vue from 'vue';
+import {router} from './routes/index.js';
+import App from './App.vue';
 
 Vue.config.productionTip = false
-
 
 new Vue({
   render: h => h(App),
